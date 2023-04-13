435fe4b56b5b114eb62b97d3c33afaa2a2dc3e4c
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-04-13 10:26:19 +0300

    redux

---
+++ src/features/newsSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    newsList : [],
+    loading: false,
+    error:false
+}
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {
+    clearNewsList:()=>{
+        state.newsList=[]
+    }
+  }
+});
+
+export const {clearNewsList} = newsSlice.actions
+
+export default newsSlice.reducer
