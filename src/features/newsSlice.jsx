d4eb391f0eb9669e0f3823e583a6915c98fcdfe7
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-04-13 14:04:37 +0300

    news created in main page

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,4 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+
 
 const initialState = {
     newsList : [],
@@ -6,13 +9,37 @@ const initialState = {
     error:false
 }
 
+export const getNews=createAsyncThunk("getNews",
+async(thunkAPI,{rejectWithValue})=>{
+    const API_KEY=process.env.REACT_APP_KEY;
+    const url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    try {
+        const {data}=await axios(url)
+        return data.articles;
+    } catch (error) {
+        console.log(error)
+        rejectWithValue("Something get wrong")
+    }
+})
+
 const newsSlice = createSlice({
   name: "news",
   initialState,
   reducers: {
     clearNewsList:(state)=>{
         state.newsList=[]
-    }
+        }
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(getNews.pending,(state)=>{
+            state.loading=true;
+        }).addCase(getNews.fulfilled,(state,{payload})=>{
+            state.newsList=payload;
+            state.loading=false;
+        }).addCase(getNews.rejected,(state)=>{
+            state.loading=false;
+            state.error=true;
+        })
   }
 });
 
